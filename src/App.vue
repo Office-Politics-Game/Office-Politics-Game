87592f06d56a305f27dc75e17ba5ac4d3870cf03
ChloeChan <Angel332332@gmail.com>
2026-06-07 14:05:18 +0800

    feat(gameRoom): 新增自訂遊戲房間頁切版

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<script setup>
-  import GameRoom from '@/components/layout/GameRoom.vue'
-</script>
-
 <template>
-  <main>
-    <section class="preview-section">
-      <GameRoom />
-    </section>
-    <RouterView />
-  </main>
+  <RouterView />
 </template>
