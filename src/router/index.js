4668454ed5af03f9d1f4bd41fa12bd4765ec4e39
Willy <willytony17@gmail.com>
2026-06-09 23:35:53 +0800

    feat: 結算成績畫面切版 補遊戲房間素材 定義main.css顏色

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Lobby from '../views/Lobby.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Lobby from "../views/Lobby.vue";
+import Result from "../views/Result.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Lobby
+    path: "/",
+    name: "Home",
+    component: Lobby,
   },
   {
-    path: '/lobby',
-    name: 'Lobby',
-    component: Lobby
+    path: "/lobby",
+    name: "Lobby",
+    component: Lobby,
   },
-]
+  {
+    path: "/result",
+    name: "Result",
+    component: Result,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
