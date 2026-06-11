4377ca6492534eef71fc938f3b570ffa10010738
maggie <h6741957g@gmail.com>
2026-06-11 10:07:05 +0800

    新增個人頁面與好友欄

--- src/components/menu/FriendItem.vue
+++ src/components/menu/FriendItem.vue
@@ -7,18 +7,15 @@
         src="@/assets/Player_1.png"
         class="w-10 h-10 rounded-full object-cover"
       />
-
       <div>
         <div class="font-medium">
           {{ name }}
         </div>
-
         <div class="text-sm text-gray-500">
           {{ status }}
         </div>
       </div>
     </div>
-
     <button
       class="px-4 py-2 bg-white rounded-md border hover:bg-gray-50"
     >
