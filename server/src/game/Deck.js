4903eabc3855a7cbf5f4ef4bd6e403beea9f43b6
ChloeChan <Angel332332@gmail.com>
2026-06-15 16:14:02 +0800

    feat: 建立初始化牌庫

---
+++ server/src/game/Deck.js
@@ -0,0 +1,26 @@
+function createDeck(){
+    return [
+        "ceo",
+        "consultant",
+        "hr",
+        "project-manager",
+        "project-manager",
+        "senior",
+        "senior",
+        "department-head",
+        "department-head",
+        "cleaner",
+        "cleaner",
+        "intern",
+        "intern",
+        "intern",
+        "intern",
+        "intern",
+    ]
+}
+
+function shuffleDeck(){
+    //洗牌
+}
+
+export { createDeck , shuffleDeck }
