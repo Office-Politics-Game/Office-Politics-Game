4966ac92d043497b00855df29a5f4d1bbd4219cd
maggie <h6741957g@gmail.com>
2026-06-15 12:31:45 +0800

    fix: 修正 Code Review 建議

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,18 @@
 import express from "express";
 import cors from "cors";
-import pool from "./db/index.js";
 
 const app = express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
 app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("server is running")
 })
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000")
-})
+const PORT = process.env.PORT;
+app.listen(PORT);
