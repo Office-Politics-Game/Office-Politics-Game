aa9126e58f9873010645c00be11faa7aad6aa9ae
LinLiWei <willytony17@gmail.com>
2026-06-20 17:27:41 +0800

    feat: 新增 GET room state API fix:  修正manager卡片素材錯字

--- server/src/routes/roomRoutes.js
+++ server/src/routes/roomRoutes.js
@@ -3,6 +3,7 @@ import {
   handleCreateRoom,
   handleJoinRoom,
   handleUpdateReady,
+  handleGetRoomState,
   handleStartGame,
 } from "../controllers/roomController.js"
 
@@ -10,6 +11,7 @@ const router = express.Router()
 
 router.post("/", handleCreateRoom)
 router.post("/:roomCode/join", handleJoinRoom)
+router.get("/:roomCode/state", handleGetRoomState)
 router.patch("/:roomCode/state", handleUpdateReady)
 router.post("/:roomCode/start", handleStartGame)
 
