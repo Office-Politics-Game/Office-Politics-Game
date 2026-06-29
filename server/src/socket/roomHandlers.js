22c6e27caea60aa812288bb84d112ae6c1e231e4
maggie <h6741957g@gmail.com>
2026-06-29 19:16:48 +0800

    feat: 新增房間 Socket 訂閱與準備狀態同步

---
+++ server/src/socket/roomHandlers.js
@@ -0,0 +1,82 @@
+import { getRoomState, updateReady } from "../services/roomService.js"
+
+function registerRoomHandlers(io, socket) {
+    socket.on("room:subscribe", async (payload, callback) => {
+        try {
+            const { roomCode, playerId } = payload
+
+            console.log("room:subscribe", roomCode, playerId)
+
+            socket.join(roomCode)
+
+            const roomState = await getRoomState({ roomCode })
+
+            io.to(roomCode).emit("room:state", roomState)
+
+            if (typeof callback === "function") {
+                callback({
+                    ok: true,
+                    data: roomState,
+                })
+            }
+        } catch (error) {
+            if (typeof callback === "function") {
+                callback({
+                    ok: false,
+                    error: {
+                        message: error.message,
+                    },
+                })
+            }
+        }
+    })
+
+    socket.on("room:unsubscribe", (payload, callback) => {
+        const { roomCode } = payload
+
+        socket.leave(roomCode)
+
+        if (typeof callback === "function") {
+            callback({
+                ok: true,
+                data: {
+                    roomCode,
+                },
+            })
+        }
+    })
+
+    socket.on("room:set-ready", async (payload, callback) => {
+        try {
+            const { roomCode, playerId, isReady } = payload
+
+            await updateReady({
+                roomCode,
+                playerId,
+                isReady,
+            })
+
+            const roomState = await getRoomState({ roomCode })
+
+            io.to(roomCode).emit("room:state", roomState)
+
+            if (typeof callback === "function") {
+                callback({
+                    ok: true,
+                    data: roomState,
+                })
+            }
+        } catch (error) {
+            if (typeof callback === "function") {
+                callback({
+                    ok: false,
+                    error: {
+                        message: error.message,
+                    },
+                })
+            }
+        }
+    })
+}
+
+export { registerRoomHandlers }
