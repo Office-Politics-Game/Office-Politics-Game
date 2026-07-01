22bdbfc1bae8832df93fe3d9fa066b2807fa7256
maggie <h6741957g@gmail.com>
2026-07-01 16:32:47 +0800

    Revert "Merge remote-tracking branch 'origin/120' into 120"
    This reverts commit abae38ca067f15470e0d6f945f96223ea9863669, reversing
    changes made to 4d049758c0d46f64d93c24d1e789f5f014002483.

--- src/services/currencyApi.js
+++
@@ -1,9 +0,0 @@
-import { apiClient } from "./apiClient.js";
-
-const PLAYER_API_PATH = "/players";
-
-function getPlayerCurrency(playerId) {
-  return apiClient.get(`${PLAYER_API_PATH}/${playerId}/currency`);
-}
-
-export { getPlayerCurrency };
