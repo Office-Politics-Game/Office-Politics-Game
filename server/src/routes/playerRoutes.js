424aaba3e1a0a0edfa724bc593acee5b90011aef
Tissue <abcd031313@gmail.com>
2026-07-01 14:13:22 +0800

    feat(friend): 串接好友前端 API

--- server/src/routes/playerRoutes.js
+++ server/src/routes/playerRoutes.js
@@ -1,8 +1,12 @@
 import express from "express"
-import { handleCreateGuest } from "../controllers/playerController.js"
+import {
+  handleCreateGuest,
+  handleSearchPlayers,
+} from "../controllers/playerController.js"
 
 const router = express.Router()
 
+router.get("/search", handleSearchPlayers)
 router.post("/guest", handleCreateGuest)
 
 export { router }
