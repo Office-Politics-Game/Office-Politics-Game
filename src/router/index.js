bf30f18eae59b11754dfd07b5901492f2d2ddbce
Shirai <p.tonki1123@gmail.com>
2026-07-22 13:50:14 +0800

    refactor:整理已不需要的裝備測試頁，刪除無用處的models資料夾

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,6 @@ import InviteFriendModal from "@/components/gameRoom/InviteFriendModal.vue";
 import CardDealDemoView from "@/views/CardDealDemoView.vue";
 import AnimationDemoView from "@/views/AnimationDemoView.vue";
 import CardPlayTestView from "@/views/CardPlayTestView.vue";
-import ProfileEquipmentTestView from "@/views/ProfileEquipmentTestView.vue";
 import StyleStudioView from "@/views/StyleStudioView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import AuthCallbackView from "@/views/AuthCallbackView.vue";
@@ -106,14 +105,6 @@ const routes = [
     path: "/profile",
     name: "Profile",
     component: ProfileView,
-  },
-  {
-    path: "/profile-equipment-test",
-    name: "ProfileEquipmentTest",
-    component: ProfileEquipmentTestView,
-    meta: {
-      requiresAuth: true,
-    },
   },
   {
     path: "/style-studio",
